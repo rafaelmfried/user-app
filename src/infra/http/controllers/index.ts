8a3fef8048b04c82591fcdf4efcc58d6747b11a2
Rafael Moura Friederick <rafael.mf.documents@gmail.com>
2026-02-24 04:32:56 -0300

    feat: add index file for HTTP controllers export

---
+++ src/infra/http/controllers/index.ts
@@ -0,0 +1,4 @@
+export type { HttpController } from "./HttpController.js";
+export { CreateUserController } from "./CreateUserController.js";
+export { ListUserController } from "./ListUserController.js";
+export { HealthCheckController } from "./HealthCheckController.js";
