9a802e25c9f58492a764c51f178107cc4b3cc120
Rafael Moura Friederick <rafael.mf.documents@gmail.com>
2026-02-24 04:32:23 -0300

    feat: create index file for user domain exports

---
+++ src/application/user/index.ts
@@ -0,0 +1,4 @@
+export { CreateUser } from "./CreateUser.js";
+export { ListUser } from "./ListUser.js";
+export { toUserDTO } from "./UserDTO.js";
+export type { CreateUserInput, UserDTO } from "./UserDTO.js";
